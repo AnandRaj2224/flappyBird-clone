c3c77c65fd223bdf11126ee32429cbbccfa3fbe1
Anand <anandraj6783@gmail.com>
2025-07-19 13:51:24 +0530

    added  flap Mechanic to the bird

--- src/index.js
+++ src/index.js
@@ -26,14 +26,19 @@ function preload() {
 }
 
 const VELOCITY = 200;
+const FLAP_VELOCITY = 250;
 let bird = null;
-
+let totalDelta = null;
 function create() {
   // Place images in the center of the canvas
   this.add.image(config.width / 2, config.height / 2, "sky");
-  bird = this.physics.add.image(config.width / 10, config.height / 2, "bird");
+  bird = this.physics.add.image(config.width * 0.1, config.height / 2, "bird").setOrigin(0);
   bird.body.gravity.y = 200;
   bird.body.velocity  = VELOCITY;
+
+  this.input.on('pointerdown', flap);
+  this.input.keyboard.on('keydown_SPACE', flap);
+
 }
 
 function update (time , delta) {
@@ -44,4 +49,7 @@ function update (time , delta) {
   }
 }
 
+function flap() {
+  bird.body.velocity = FLAP_VELOCITY;
+}
 new Phaser.Game(config);
