5a7170478f644c89f425a1ddb4bd0c77b3c65d3c
Anand <anandraj6783@gmail.com>
2025-07-19 13:21:34 +0530

    added a update function to update scene and default gravity to bird object

--- src/index.js
+++ src/index.js
@@ -5,26 +5,37 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade'
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+    },
   },
   scene: {
     preload: preload,
-    create: create
-  }
+    create: create,
+    update : update,
+  },
 };
 
 // Loading assets like images, music, etc.
 function preload() {
   // 'this' context -- scene
   // Contains functions and properties we can use
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('bird', 'assets/bird.png'); 
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("bird", "assets/bird.png");
 }
+
+let bird = null;
 
 function create() {
   // Place images in the center of the canvas
-  this.add.image(config.width / 2, config.height / 2, 'sky'); 
-  this.add.image(config.width / 2, config.height / 2, 'bird'); 
+  this.add.image(config.width / 2, config.height / 2, "sky");
+  bird = this.physics.add.image(config.width / 10, config.height / 2, "bird");
+  bird.body.gravity.y = 200;
+}
+
+function update () {
+  
 }
 
 new Phaser.Game(config);
