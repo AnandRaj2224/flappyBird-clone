9f575ec416136810c20878fe61ed39bdba1d1d85
Anand <anandraj6783@gmail.com>
2025-07-20 20:43:05 +0530

    added best score

--- src/scenes/playScene.js
+++ src/scenes/playScene.js
@@ -68,7 +68,9 @@ class PlayScene extends Phaser.Scene {
 
   createScore() {
     this.score = 0;
+    const bestScore = localStorage.getItem('bestScore');
     this.scoreText = this.add.text(16,16,`Score: ${0}`, {fontSize : '32px', fill : '#000'});
+    this.add.text(16,52,`Best Score: ${bestScore|| 0}`, {fontSize : '18px', fill : '#000'});
   }
 
   handleInputs() {
@@ -105,6 +107,7 @@ class PlayScene extends Phaser.Scene {
         if(tempPipes.length === 2) {
           this.placePipes(...tempPipes);
           this.increaseScore();
+          this.saveBestScore();
         }
       }
     });
@@ -118,9 +121,20 @@ class PlayScene extends Phaser.Scene {
     return rightMostPipe;
   }
   
+  saveBestScore() {
+    const bestScoreText = localStorage.getItem('bestScore');
+    const bestScore = bestScoreText && parseInt(bestScoreText,10);
+
+    if(!bestScore || this.score > bestScore) {
+      localStorage.setItem('bestScore',this.score);
+    }
+  }
+
   gameOver() {
     this.physics.pause();
     this.bird.setTint(0xEE4824);
+
+    this.saveBestScore();
 
     this.time.addEvent({
       delay : 1000,
