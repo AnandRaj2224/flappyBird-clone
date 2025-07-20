d92ffc471736a8176afb80c05b06a2938ef3ce03
Anand <anandraj6783@gmail.com>
2025-07-20 21:40:34 +0530

    created a score scene

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import Phaser from "phaser";
 import PlayScene from "./scenes/playScene";
 import MenuScene from './scenes/menuScene';
 import PreloadScene from './scenes/preloadScene';
+import ScoreScene from './scenes/scoreScene';
+
 
 
 const WIDTH = 800;
 const HEIGHT = 600;
 const BIRD_POSITION = {x: WIDTH * 0.1, y : HEIGHT / 2};
 
-const Scenes = [PreloadScene, MenuScene, PlayScene];
+const Scenes = [PreloadScene, MenuScene, ScoreScene, PlayScene];
 const createScene = Scene => new Scene(SHARED_CONFIG);
 const initScenes = () => Scenes.map(createScene);
 
